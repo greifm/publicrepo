c8fd86113d9a6a6b3936bab560d81e41135f6b6a
greifm <59001319+greifm@users.noreply.github.com>
2020-08-28 04:07:06 +0000

    Update benchmarkMagnus.sh

--- benchmarkMagnus.sh
+++ benchmarkMagnus.sh
@@ -2,12 +2,15 @@
 
 # a program to benchmark gromacs md on magnus
 # place this in a dir with all required files
-# requires a job file with variables nodes_var and cores_var in the file jobsub.sh
+# requires a job file with variables nodes_var cores_var, time_var in the file jobsub.sh
 # requires the jobsub.sh file and all files required to run your sim in a dir called files
+# jobsub.sh contents at end of file
 
 # define nodes to test 
-#   all other defining (such as time) is taken from the jobsub.sh file
-nodes=(1 2 3 4 5 6 7 8 9 10 11 12)
+#   all other defining is taken from the jobsub.sh file
+
+nodes=(1) # 2 3 4 5 6 7 8 9 10 11 12)
+time_in=15 # in minutes
 
 for ii in ${nodes[*]}
 do
@@ -17,13 +20,14 @@ do
     sed -i "s/nodes_var/$ii/g" jobsub.sh
     let cores=($ii * 24)
     sed -i "s/cores_var/$cores/g" jobsub.sh
+    sed -i "s/time_var/$time_in/g" jobsub.sh
     cd ..
 done
 
 # submit 
 echo '############ Listing files ###############'
 for ii in $nodes; do echo $ii; ls $ii; done
-echo "Would you like to submit?
+echo "Would you like to submit?"
 read nothing
 
 for ii in ${nodes[*]}
@@ -32,3 +36,26 @@ do
     sbatch jobsub.sh
     cd ..
 done
+
+jobsubsh ()
+{
+#!/bin/bash -l
+#SBATCH --account=pawsey0110
+#SBATCH --job-name=Coacervate_bench
+#SBATCH --partition=workq
+#SBATCH --time=00:time_var:00
+#SBATCH --nodes=nodes_var
+#SBATCH --export=ALL
+#======START=====
+module load slurm
+echo "The current job ID is $SLURM_JOB_ID"
+echo "Running on $SLURM_JOB_NUM_NODES nodes"
+echo "Using $SLURM_NTASKS_PER_NODE tasks per node"
+echo "A total of $SLURM_NTASKS tasks is used"
+echo "Node list:"
+sacct --format=JobID,NodeList%100 -j $SLURM_JOB_ID
+module swap PrgEnv-cray PrgEnv-gnu
+module load gromacs/2018.3
+let "time_max = time_var / 60 - 0.05"
+srun --export=all -N nodes_var -n cores_var mdrun_mpi -s OLysRNA_min_eq_prod.tpr -c coacervate_prod0.gro -v -g log.log -x traj_comp.xtc -maxh $time_max
+}
